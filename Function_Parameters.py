e3c4c2100a83625ae4a441b1f225afd4cb19727d
Ray Sylverne <raysylverne@gmail.com>
2022-12-27 12:17:34 -0500

    Encapsulating Logic within a function

--- Function_Parameters.py
+++ Function_Parameters.py
@@ -4,6 +4,7 @@ Functions are defined using the "def" keyword
 Functions are like variables except an entire block of code can be placed into a function
 '''
 
+''' 
 calculation_to_units = 24
 name_of_unit = "hours"
 
@@ -11,22 +12,29 @@ name_of_unit = "hours"
 def days_to_units():
     print(f"20 Days are {20 * calculation_to_units} {name_of_unit}")
     print("All good!")
+    
+day_to_units()
+'''
 
 # A function contains a Block of code which only runs when it is called
 # The round () brackets at the end of the string lets python know we are calling a function
 
 
-# Function Parameters
+
+               # Adding Input Parameters to a Function
 # Information can be passed into functions as parameters
 # Parameters are also called arguments (input)
 
 
-'''
 calculation_to_units = 24
 name_of_unit = "hours"
 
 
-def days_to_units():
-    print(f"20 Days are {20 * calculation_to_units} {name_of_unit}")
-    print("All good!")
-'''
+def days_to_units(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+    print(custom_message)
+
+
+days_to_units(20, "Awesome!")
+days_to_units(30, "Looks Good!")
+
