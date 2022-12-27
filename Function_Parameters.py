915e8b89b20c3fa02106ddd8b40c3ccda61b4dd2
Ray Sylverne <raysylverne@gmail.com>
2022-12-27 12:43:37 -0500

    #This area is known as the function body

--- Function_Parameters.py
+++ Function_Parameters.py
@@ -46,7 +46,7 @@ name_of_unit = "hours"
 
 # Add your variable within the rounded brackets and separate with a comma
 # Add the newly created variable within your function (custom_message)
-
+'''
 def days_to_units(num_of_days, custom_message):
     print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
     print(custom_message)
@@ -54,3 +54,24 @@ def days_to_units(num_of_days, custom_message):
 days_to_units(20, "Awesome")
 days_to_units(30, "Looks Great")
 days_to_units(40, "Job Well Done!")
+'''
+
+# Variable Scope and Function
+# A variable is only available from inside the region it is created
+# -Global Variable = variable available from within any scope
+# -Local Variable = variables created inside a function and only available to that function
+
+def days_to_units(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+    print(custom_message)
+
+
+# Local Variables like num_of_days are only availble within the function is was created
+# Hence the name local. If you try to use it outside of that function you will get and error
+# You would have to redefine the variable in the new function before you could call on it
+
+def scope_check():
+    print(name_of_unit)    #This area is known as the function body
+    print(num_of_days)     #This area is known as the function body
+
+scope_check()
