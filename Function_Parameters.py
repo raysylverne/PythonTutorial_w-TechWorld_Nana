62f37a36d097c5ec1be919ad0c181d7438a06f9c
Ray Sylverne <raysylverne@gmail.com>
2022-12-27 12:04:39 -0500

    Added function parameters

---
+++ Function_Parameters.py
@@ -0,0 +1,32 @@
+# Introducing the use of functions
+'''
+Functions are defined using the "def" keyword
+Functions are like variables except an entire block of code can be placed into a function
+'''
+
+calculation_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units():
+    print(f"20 Days are {20 * calculation_to_units} {name_of_unit}")
+    print("All good!")
+
+# A function contains a Block of code which only runs when it is called
+# The round () brackets at the end of the string lets python know we are calling a function
+
+
+# Function Parameters
+# Information can be passed into functions as parameters
+# Parameters are also called arguments (input)
+
+
+'''
+calculation_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units():
+    print(f"20 Days are {20 * calculation_to_units} {name_of_unit}")
+    print("All good!")
+'''
