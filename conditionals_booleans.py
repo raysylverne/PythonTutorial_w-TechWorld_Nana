6dac064364821ed8c2f13a702f27ac8913a30185
Ray Sylverne <raysylverne@gmail.com>
2022-12-28 23:50:47 -0500

    added else statement that provides output for user if a negative # is passed to function "Please provide positive number for the conversion to work!"

--- conditionals_booleans.py
+++ conditionals_booleans.py
@@ -3,6 +3,8 @@ name_of_unit = "hours"
 def days_to_units(num_of_days):
     if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    else:
+        return "Please provide positive number for the conversion to work!"
 
 
 user_input = input("Hey there, enter a number of days and I will convert it to hours: ")
