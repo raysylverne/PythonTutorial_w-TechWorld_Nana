d01a8598a3bc3b5198e6f2e9b8dbb02ddb314dec
Ray Sylverne <raysylverne@gmail.com>
2022-12-29 00:12:13 -0500

    Nested the int function in the days_to_units(int(user_input)) to alleviate the need for the user_input_number variable

--- conditionals_booleans.py
+++ conditionals_booleans.py
@@ -1,7 +1,8 @@
 calculation_to_units = 24
 name_of_unit = "hours"
 def days_to_units(num_of_days):
-    if num_of_days > 0:
+    print(num_of_days > 0)
+    if num_of_days > 0:     # this is a conditional that cna be true or false
         return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
     else:
         return "Please provide positive number for the conversion to work!"
@@ -9,15 +10,21 @@ def days_to_units(num_of_days):
 
 user_input = input("Hey there, enter a number of days and I will convert it to hours: ")
 
-user_input_number = int(user_input)
-calculated_value = days_to_units(user_input_number)
+calculated_value = days_to_units(int(user_input))
 print(calculated_value)
 
 
 
 ''' NOTES
-This will focus on restriction and validation of user input that logically doesn't make sense, could crash our program  or could be a security risk. 
-Line 4: Is the user input a positive number. Note: You have to indent the "return statement" so that it is evaluated by the "if statement" Or it will error out
+This will focus on restriction and validation of user input that logically doesn't make sense, could crash our program  
+or could be a security risk. 
+Line 5: Is the user input a positive number. Note: You have to indent the "return statement" so that it is evaluated by 
+the "if statement" Or it will error out
+Line 7: If user provides a negative # the else statement will return output that provides guidance
+line 13: By nesting a function within a function days_to_units(int(user_input)) we've alleviated the need for the 
+user_input_number variable
+L 13: The Return value of the inner function is the input value for the outer function "days_to_units(int(user_input))"
+
 '''
 
 ''' Expressions that evaluate to either true or false
