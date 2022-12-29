5b509604871e599b342b34ee31c747883cd4150c
Ray Sylverne <111945210+raysylverne@users.noreply.github.com>
2022-12-28 23:26:35 -0500

    Added clarity to notes
    Added clarity to notes  in case I need to come back to these notes

--- Userinput.py
+++ Userinput.py
@@ -8,19 +8,23 @@ name_of_unit = "hours"
 def days_to_units(num_of_days):
     return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
-# input() value by default is always treated as a string. In this scenario if you don't
-# it will simply display the inputted number 24 times "1010101" instead multiplying by 24
+# input() value by default is always treated as a string. In this scenario if you don't use the int() function
+# it will simply display the inputted number 24 times "1010101" instead multiplying by 24. See final comments for
+# full explanation (lines25-29)
+
 user_input = input("Hey there, enter a number of days and I will convert it to hours: \n")
+
 # Encapsulating the variable user_input in the int is called casting.
 # The int in python is a built-in method that converts a string or a number into an integer
-user_input_number = int(user_input)
 
+
+user_input_number = int(user_input)
 calculated_value = days_to_units(user_input_number)
 print(calculated_value)
 
-'''Realized that function wasn't working due to the days_to_units function was treated num_
-of_days as a string. Created the user_input_number variable that converts it to an intenge 
-by calling the built-in int function. Now when the calculated_value variable calls the 
+'''Realized that function wasn't working due to the days_to_units function treating num_
+of_days as a string insead of an integer. Created the user_input_number variable that converts the user_input 
+to an integer by calling the int() function. Now when the calculated_value variable calls the 
 "days_to_units(user_input_number)" the code works since its treating num_of_days aka 
 user_input_number as an integer instead of a string
 '''
