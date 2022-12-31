1ff2a240533647642606ff32e5fda4253bebb949
Ray Sylverne <raysylverne@gmail.com>
2022-12-30 22:15:46 -0500

    Created module using the following syntax (import dict_helper)

--- dict_module.py
+++ dict_module.py
@@ -1,28 +1,23 @@
-def days_to_units(num_of_days, conversion_unit):
-    if conversion_unit == "hours":
-        return f"{num_of_days} days are {num_of_days * 24} hours"
-    elif conversion_unit == "minutes":
-        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
-    elif conversion_unit == "seconds":
-        return f"{num_of_days} days are {num_of_days * 24 * 60 * 60 } seconds"
-    else:
-        return "unsupported unit"
+'''
+Created two files dict_module.py and dict_helper.py in order to practice using modules. The code in dict_module.py
+came from dictionarydatatypes.py
+A module is a python file that contains funtions and variables that can be used by another python file within your
+project. Essentially any python file within your file can become a module. The idea is that you can structure your
+program using modules making your project modular. We will create our own module in the dict_helper.py file, and we're
+going to reference in  dict_module.py.
+'''
+
+#END GOAL#
+# 1- In dict_module.py I only want the code that starts the program.
+# 2- Move and group all the functions, and logic in the dict_helper.py file
+# 3- Use Modules to connect the two files
 
 
-def validate_and_execute():
-    try:
+''' 
+#There are two ways to connect two files the first way shown here brings the entire file over using the following
+#syntax import <filename>  can be seen below
 
-        user_input_number = int(days_and_unit_dictionary["days"])
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("You entered a 0, please enter a valid positive number")
-        else:
-            print("You entered a negative number, please enter a positive number")
-    except ValueError:
-        print("Your input is not a number don't crash the program")
-
+import dict_helper
 
 user_input = ""
 while user_input != "exit":
@@ -30,5 +25,22 @@ while user_input != "exit":
                        "Conversion unit can be seconds, minutes our hours.\n"
                        "Exp input (45:hours) Enter text here: ")
     days_and_unit = user_input.split(':')
+    print(days_and_unit)
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
-    validate_and_execute()
+    dict_helper.validate_and_execute(days_and_unit_dictionary)
+'''
+
+
+#The second way to connect tow files involves only brininging over select functions and variables instead of the
+# entire file see below for syntax
+
+
+from dict_helper import validate_and_execute, user_input_message
+
+
+user_input = ""
+while user_input != "exit":
+    user_input = input(user_input_message)
+    days_and_unit = user_input.split(':')
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    validate_and_execute(days_and_unit_dictionary)
