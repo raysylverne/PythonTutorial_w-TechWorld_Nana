94dc144d4ee7a02de063ae6754fcff6ff5e0b4ce
Ray Sylverne <raysylverne@gmail.com>
2023-01-03 19:51:43 -0500

    hacker

--- automate_excelfile.py
+++ automate_excelfile.py
@@ -17,11 +17,14 @@ for product_row in range(2, product_list.max_row + 1):
         current_num_products = products_per_supplier_dict.get(supplier_name)
         products_per_supplier_dict[supplier_name] = current_num_products + 1
     else:
-        print("adding a new supplier")
         products_per_supplier_dict[supplier_name] = 1
 
-        # calcualte total value of inventory per supplier
-    if supplier_name in total_value_per_supplier
-        total_value_per_supplier.get(supplier_name)
-        total_value_per_supplier = inventory * price
+      # calculate total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
 
+print(products_per_supplier_dict)
+print(total_value_per_supplier)
