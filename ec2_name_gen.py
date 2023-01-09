39550cc38e16a946433a78afa801ac5cb8faded8
Ray Sylverne <raysylverne@gmail.com>
2023-01-09 15:01:21 -0500

    updated file names for orginal ec2_gen file that has no functions. Along with updated version that imports a module where all the logic is stored

--- ec2_name_gen.py
+++ ec2_name_gen.py
@@ -1,56 +1,35 @@
+
 '''
-Several departments share an AWS environment. You need to ensure that the EC2s are properly named and are unique
-so team members can easily tell who the EC2 instances belong to. Use Python to create your unique EC2 names that the
-users can then attach to the instances. The Python Script should:
-
-1. All the user to input how many EC2 instances they want names for and output the same amount of unique names.
-2. Allow the user to input the name of their department that is used in the unique name.
-3. Generate random characters and numbers that will be included in the unique name.
-
-ADVANCED
-The only departments that should use this Name Generator are the Marketing, Accounting, and FinOps Departments.
-List these departments as options and if a user puts another department, print a message that they should not use this
-Name Generator. Be sure to account for incorrect upper or lowercase letters in the correct department.
-Example: a user inputs accounting vs Accounting.
-
-COMPLEX
-Turn the above into a Function and execute the Function to verify it works.'''
-
-from random import random
-from uuid import uuid4
+Finished EC2 name gen, now I want to create a while loop that alleviates the need to re-run the app every time its
+needed. I also want to move all the logic to a sep module that will be call upon
+'''
+import sys
+from ec2_name_gen_helper import no_negative, validate_dept_name, uuid4_generator
 
 
-def string_generator():
-    return str(uuid4())
+department_name = ""
+number_of_ec2 = ""
+while True:
 
-# Allow the user to input the name of their department that is used in the unique name.
-department_name = input("Enter Department: Marketing, Accounting, FinOps: ")
-for i in department_name:
-    if department_name == "Marketing" or department_name == "marketing":
-        print(f"We can always rely on {department_name} department to follow company naming convention")
-        break
-    elif department_name == "Accounting" or "accounting" == department_name:
-        print(f"We can always rely on {department_name} department to follow company naming convention")
-        break
-    elif department_name == "FinOps" or "finOps" == department_name:
-        print(f"We can always rely on {department_name} department to follow company naming convention")
-        break
+    department_valid = False
+    while not department_valid:
+        department_name = input("Enter Department: Marketing, Accounting, FinOps (or type exit): ")
+        department_valid = validate_dept_name(department_name)
+        if department_name == 'exit':
+            sys.exit()
 
-    else:
-        print("Error: In order to use this generator, you enter the name correctly.\n"
-              "Example: Marketing, Accounting, FinOps")
-        exit()
+    number_of_ec2 = int(input("How many EC2 instances do you require?: "))
+    no_negative(number_of_ec2)
+    for _ in range(1, number_of_ec2 + 1):
+        unique_name = department_name
+        EC2_unique_name = unique_name + "-" + uuid4_generator()
+        print("Your EC2 Instance's unique name is: ", EC2_unique_name)
+    more_instances = input("Do you need additional unique EC2 instances names created(Y/N): ")
+    if more_instances == "Y" or "y" == more_instances: # exit if user no longer requires the program to run
+        print("-----------------------------------------------------------------------------------")
+        print("")
+        continue # if true -> continue while loop
+    else: # if false -> break
+        sys.exit()
 
-# All the user to input how many EC2 instances they want names for and output the same amount of unique names.
-number_of_ec2 = int(input("How many EC2 instances do you require?\n"))
-if number_of_ec2 < 0:
-    print("Please enter a positive number: ")
-elif number_of_ec2 > 0:
-    print(f"Creating {number_of_ec2} unique EC2 Instance Names")
-
-# Generate random characters and numbers that will be included in the unique name.
-for _ in range(1, number_of_ec2 + 1):
-    unique_name = department_name
-    EC2_unique_name = unique_name + "-" + string_generator()
-    print("Your EC2 Instance's unique name is : ", EC2_unique_name)
 
