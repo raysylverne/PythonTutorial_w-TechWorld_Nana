2fc462e07b1357bd20ca1e6013fc1534552d6295
Ray Sylverne <raysylverne@gmail.com>
2023-01-09 14:38:56 -0500

    Finished ec2 name generator. I created a copy of code in order to create a while loop that lets user run the good as needed and terminates if "exit" argument is passed to department_name

--- ec2test_name_gen.py
+++ ec2test_name_gen.py
@@ -3,46 +3,33 @@
 Finished EC2 name gen, now I want to create a while loop that alleviates the need to re-run the app every time its
 needed. I also want to move all the logic to a sep module that will be call upon
 '''
-
-from uuid import uuid4
-
-
-def string_generator():
-    return str(uuid4())
+import sys
+from ec2_helper import no_negative, validate_dept_name, uuid4_generator
 
 
-def validate_dept_name(department):
-    for i in department:
-        if department == "Marketing" or department == "marketing":
-            print(f"We can always rely on {department} department to follow company naming convention")
-            break
-        elif department == "Accounting" or "accounting" == department:
-            print(f"We can always rely on {department} department to follow company naming convention")
-            break
-        elif department == "FinOps" or "finOps" == department:
-            print(f"We can always rely on {department} department to follow company naming convention")
-            break
-    else:
-        return ("Error: In order to use this generator, you enter the name correctly. "
-                "Example: Marketing, Accounting, FinOps")
-        exit()
+department_name = ""
+number_of_ec2 = ""
+while True:
 
+    department_valid = False
+    while not department_valid:
+        department_name = input("Enter Department: Marketing, Accounting, FinOps (or type exit): ")
+        department_valid = validate_dept_name(department_name)
+        if department_name == 'exit':
+            sys.exit()
 
-def no_negative(required_ec2):
-    if required_ec2 < 0:
-        print("Please enter a positive number: ")
-    elif required_ec2 > 0:
-        print(f"Creating {required_ec2} unique EC2 Instance Names")
-
-
-department_name = input("Enter Department: Marketing, Accounting, FinOps: ")
-number_of_ec2 = int(input("How many EC2 instances do you require?\n"))
-while number_of_ec2 == "exit":
-    validate_dept_name(department_name)
+    number_of_ec2 = int(input("How many EC2 instances do you require?: "))
     no_negative(number_of_ec2)
     for _ in range(1, number_of_ec2 + 1):
         unique_name = department_name
-        EC2_unique_name = unique_name + "-" + string_generator()
-        print("Your EC2 Instance's unique name is : ", EC2_unique_name)
-    else:
-        exit()
+        EC2_unique_name = unique_name + "-" + uuid4_generator()
+        print("Your EC2 Instance's unique name is: ", EC2_unique_name)
+    more_instances = input("Do you need additional unique EC2 instances names created(Y/N): ")
+    if more_instances == "Y" or "y" == more_instances: # exit if user no longer requires the program to run
+        print("-----------------------------------------------------------------------------------")
+        print("")
+        continue # if true -> continue while loop
+    else: # if false -> break
+        sys.exit()
+
+
